5f027b8a6a13189508135818d32ec50a8cf6d8d2
alexpintodiaz <alexanderp143@hotmail.com>
2021-06-03 21:08:08 -0500

    Taller_5_Ejercicio_1

---
+++ main.js
@@ -0,0 +1,11 @@
+let suma = 0;
+let x;
+let list =[];
+
+for (x=1; x<=10; x++){
+    suma += x;
+    list.push(x);
+}
+
+alert(`La suma de los numeros: ${list} es ${suma}`);
+console.log(`La suma de los numeros: ${list} es ${suma}`);
